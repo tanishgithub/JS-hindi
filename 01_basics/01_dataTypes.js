0a38de8b713604010cc129ae7d197ed34b2de329
tanishgithub <109534297+tanishgithub@users.noreply.github.com>
2023-08-23 04:09:37 +0000

    new commit datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //new JS used 
+let name = "Maruti"
+let age = 10
+let isLoggedIn = true
+
+//number
+//bigint
+//string
+//boolean
+//undefined => no value assigned yet
+//null => an object specifying empty
+// symbol => unique (used in react)
+
+console.log(typeof age);
+console.log(typeof null)
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14452
+let accountEmail = "tanishsingh@google.com"
+var accountPassword = "12345"
+accountCity = "Bhopal"
+let accountState;
+
+// accountId = 2 // Not allowed
+console.log(accountId);
+
+accountEmail = "tpsc@tpsc"
+accountPassword = "6789"
+accountCity = "Delhi"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
